404c7233c39d5ec378b1f1b768bbd62a17f4ca94
Christian <cfs.christianfuentes@gmail.com>
2022-04-04 15:10:55 -0500

    Initial commit

---
+++ app/src/main/java/com/example/pokemon/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.pokemon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.example.pokemon.databinding.ActivityMainBinding
+import org.json.JSONObject
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var queue:RequestQueue
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        queue = Volley.newRequestQueue(this)
+
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnUpdatePokemon.setOnClickListener {
+            getPokemonList(binding.etPokemonAmount.text.toString().toInt())
+        }
+    }
+
+    fun getPokemonList(listAmount: Int){
+        val url = "https://pokeapi.co/api/v2/pokemon/?limit=${listAmount}"
+
+        val jsonRequest = JsonObjectRequest(url, Response.Listener<JSONObject>{
+            Log.i("JSONRESPONSE", it.getJSONArray("results").toString())
+        },
+        Response.ErrorListener {
+            Log.w("JSONRESPONSE", it.message as String)
+        })
+
+        queue.add(jsonRequest)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        queue.cancelAll("Stopped")
+    }
+}
